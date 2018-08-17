5a78c16dce592db1acd61b84c827225c19a5fd51
Mbeng <mbengtanyi@ymail.com>
2018-08-17 08:21:44 +0100

    Pushing some piece of code to github :-)

---
+++ exercise.py
@@ -0,0 +1,11 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> hello
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    hello
+NameError: name 'hello' is not defined
+>>> 'hello'
+'hello'
+>>> 
